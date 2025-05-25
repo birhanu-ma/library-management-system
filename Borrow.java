ce009495f2c8d4145ac4ed2365e2f7f7b4aa4837
Birhanu <birhanumatebe12@gmail.com>
2025-05-25 15:57:28 +0300

    borrow file is added

---
+++ Borrow.java
@@ -0,0 +1,46 @@
+import java.time.LocalDateTime;
+
+public class Borrow {
+    int borrowId;
+    String userId;
+    String bookId;
+    LocalDateTime borrowDate;
+    LocalDateTime dueDate;
+    LocalDateTime returnDate;
+
+    private static int idCounter = 1;
+
+    public Borrow(String userId, String bookId, int day) {
+        this.borrowId = idCounter++;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.borrowDate = LocalDateTime.now();
+        this.dueDate = LocalDateTime.now().plusDays(day);
+        this.returnDate = null;
+    }
+
+
+    public int getBorrowId() {
+        return borrowId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public LocalDateTime getBorrowDate() {
+        return borrowDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+}
