6af4e1f41ab0d09a51809d7569a309838996a0e4
Birhanu <birhanumatebe12@gmail.com>
2025-05-25 16:00:37 +0300

    user file is added

---
+++ User.java
@@ -0,0 +1,20 @@
+
+enum UserStatus {
+    ACTIVE,
+    WARNING,
+    SUSPENDED
+}
+
+public class User {
+
+    String userName;
+    String userId;
+    UserStatus userStatus;
+
+    public User(String userID, String userName) {
+        this.userId = userID;
+        this.userName = userName;
+        this.userStatus = UserStatus.ACTIVE;
+
+    }
+}
